55788dccf4ec4fcbbc82e8aaa0735922a30dff9e
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-12-10 16:32:07 +0000

    Increase max known version (#25)

--- PyMCTCompiler/versions/java_1_21_9/__init__.py
+++ PyMCTCompiler/versions/java_1_21_9/__init__.py
@@ -4,7 +4,7 @@ import os
 compiler = JavaBlockstateCompiler(
     os.path.dirname(__file__),
     version=[1, 21, 9],
-    version_max_known=[1, 21, 10],
+    version_max_known=[1, 21, 11],
     version_max=[2, -1],
     parent_version="java_1_21_8",
     data_version=4553,
