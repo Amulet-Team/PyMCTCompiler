83ffd61fab1a9d8aa6d265f2e560caaae4578a24
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2026-03-25 11:41:41 +0000

    Bump max version

--- PyMCTCompiler/versions/bedrock_1_21_110/__init__.py
+++ PyMCTCompiler/versions/bedrock_1_21_110/__init__.py
@@ -4,8 +4,8 @@ import os
 compiler = NBTBlockstateCompiler(
     os.path.dirname(__file__),
     version=[1, 21, 110],
-    version_max_known=[1, 21, 130],
-    version_max=[2, -1],
+    version_max_known=[26, 3],
+    version_max=[26, 10, -1],
     parent_version="bedrock_1_21_100",
     data_version=18168865,
     data_version_max_known=18168865,
