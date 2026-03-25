2478516b0b67f1a5996cbd2955f386aaf2bae459
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2026-03-25 14:54:08 +0000

    Fix flower pot conversions

--- PyMCTCompiler/primitives/scripts/nbt/flower_pot.py
+++ PyMCTCompiler/primitives/scripts/nbt/flower_pot.py
@@ -292,7 +292,7 @@ b_plants_19 = {
     '"lily_of_the_valley"': ("minecraft:red_flower", 10),
 }
 
-b_blockstate_plants = {
+b_blockstate_plants_113 = {
     '"dandelion"': ("minecraft:yellow_flower", "{}", 17629184),
     '"poppy"': ("minecraft:red_flower", '{flower_type: "poppy"}', 17629184),
     '"blue_orchid"': ("minecraft:red_flower", '{flower_type: "orchid"}', 17629184),
@@ -307,6 +307,26 @@ b_blockstate_plants = {
     '"white_tulip"': ("minecraft:red_flower", '{flower_type: "tulip_white"}', 17629184),
     '"pink_tulip"': ("minecraft:red_flower", '{flower_type: "tulip_pink"}', 17629184),
     '"oxeye_daisy"': ("minecraft:red_flower", '{flower_type: "oxeye"}', 17629184),
+    '"red_mushroom"': ("minecraft:red_mushroom", "{}", 17629184),
+    '"brown_mushroom"': ("minecraft:brown_mushroom", "{}", 17629184),
+    '"fern"': ("minecraft:tallgrass", '{tall_grass_type: "fern"}', 17629184),
+    '"dead_bush"': ("minecraft:deadbush", "{}", 17629184),
+    '"cactus"': ("minecraft:cactus", "{age: 0}", 17629184),
+    '"bamboo"': (
+        "minecraft:bamboo",
+        '{age_bit: 0, bamboo_leaf_size: "no_leaves", bamboo_stalk_thickness: "thin"}',
+        17629184,
+    ),
+    '"cornflower"': ("minecraft:red_flower", '{flower_type: "cornflower"}', 17629184),
+    '"lily_of_the_valley"': (
+        "minecraft:red_flower",
+        '{flower_type: "lily_of_the_valley"}',
+        17629184,
+    ),
+    '"wither_rose"': ("minecraft:wither_rose", "{}", 17629184),
+}
+
+b_blockstate_plants_113_saplings = {
     '"oak_sapling"': (
         "minecraft:sapling",
         '{age_bit: 0b, sapling_type: "oak"}',
@@ -337,30 +357,49 @@ b_blockstate_plants = {
         '{age_bit: 0b, sapling_type: "dark_oak"}',
         17629184,
     ),
-    '"red_mushroom"': ("minecraft:red_mushroom", "{}", 17629184),
-    '"brown_mushroom"': ("minecraft:brown_mushroom", "{}", 17629184),
-    '"fern"': ("minecraft:tallgrass", '{tall_grass_type: "fern"}', 17629184),
-    '"dead_bush"': ("minecraft:deadbush", "{}", 17629184),
-    '"cactus"': ("minecraft:cactus", "{age: 0}", 17629184),
-    '"bamboo"': (
-        "minecraft:bamboo",
-        '{age_bit: 0, bamboo_leaf_size: "no_leaves", bamboo_stalk_thickness: "thin"}',
-        17629184,
-    ),
-    '"cornflower"': ("minecraft:red_flower", '{flower_type: "cornflower"}', 17629184),
-    '"lily_of_the_valley"': (
-        "minecraft:red_flower",
-        '{flower_type: "lily_of_the_valley"}',
-        17629184,
-    ),
-    '"wither_rose"': ("minecraft:wither_rose", "{}", 17629184),
 }
 
-b_blockstate_plants_16 = {
+b_blockstate_plants_116 = {
     '"crimson_fungus"': ("minecraft:crimson_fungus", "{}", 17760256),
     '"crimson_roots"': ("minecraft:crimson_roots", "{}", 17760256),
     '"warped_fungus"': ("minecraft:warped_fungus", "{}", 17760256),
     '"warped_roots"': ("minecraft:warped_roots", "{}", 17760256),
+}
+
+b_blockstate_plants_117 = {
+    '"azalea_bush"': ("minecraft:azalea", "{}", 17879555),
+    '"flowering_azalea_bush"': ("minecraft:flowering_azalea", "{}", 17879555),
+}
+
+b_blockstate_plants_119 = {
+    '"mangrove_propagule"': (
+        "minecraft:mangrove_propagule",
+        "{hanging: 0b, propagule_stage: 0}",
+        17959425,
+    ),
+}
+
+b_blockstate_plants_11980 = {
+    '"cherry_sapling"': ("minecraft:cherry_sapling", "{age_bit: 0b}", 18042891),
+}
+
+b_blockstate_plants_12080 = {
+    '"oak_sapling"': ("minecraft:oak_sapling", "{age_bit: 0b}", 17629184),
+    '"spruce_sapling"': ("minecraft:spruce_sapling", "{age_bit: 0b}", 17629184),
+    '"birch_sapling"': ("minecraft:birch_sapling", "{age_bit: 0b}", 17629184),
+    '"jungle_sapling"': ("minecraft:jungle_sapling", "{age_bit: 0b}", 17629184),
+    '"acacia_sapling"': ("minecraft:acacia_sapling", "{age_bit: 0b}", 17629184),
+    '"dark_oak_sapling"': ("minecraft:dark_oak_sapling", "{age_bit: 0b}", 17629184),
+}
+
+b_blockstate_plants_12150 = {
+    '"open_eyeblossom"': ("minecraft:open_eyeblossom", "{}", 18163713),
+    '"closed_eyeblossom"': ("minecraft:closed_eyeblossom", "{}", 18163713),
+    '"pale_oak_sapling"': ("minecraft:pale_oak_sapling", "{age_bit: 0b}", 18163713),
+}
+
+b_blockstate_plants_2610 = {
+    '"golden_dandelion"': ("minecraft:golden_dandelion", "{}", 18168865),
 }
 
 _J19 = TranslationFile(
@@ -383,14 +422,51 @@ _B19 = TranslationFile(
     pot_item_from_universal_numerical_bedrock(b_plants_19),
 )
 
-_B113 = TranslationFile(
-    pot_item_to_universal_blockstate_bedrock(b_blockstate_plants),
-    pot_item_from_universal_blockstate_bedrock(b_blockstate_plants),
+_B113_base = TranslationFile(
+    pot_item_to_universal_blockstate_bedrock(b_blockstate_plants_113),
+    pot_item_from_universal_blockstate_bedrock(b_blockstate_plants_113),
+)
+
+_B113_saplings = TranslationFile(
+    pot_item_to_universal_blockstate_bedrock(b_blockstate_plants_113_saplings),
+    pot_item_from_universal_blockstate_bedrock(b_blockstate_plants_113_saplings),
 )
 
 _B116 = TranslationFile(
-    pot_item_to_universal_blockstate_bedrock(b_blockstate_plants_16),
-    pot_item_from_universal_blockstate_bedrock(b_blockstate_plants_16),
+    pot_item_to_universal_blockstate_bedrock(b_blockstate_plants_116),
+    pot_item_from_universal_blockstate_bedrock(b_blockstate_plants_116),
+)
+
+_B117 = TranslationFile(
+    pot_item_to_universal_blockstate_bedrock(b_blockstate_plants_117),
+    pot_item_from_universal_blockstate_bedrock(b_blockstate_plants_117),
+)
+
+_B119 = TranslationFile(
+    pot_item_to_universal_blockstate_bedrock(b_blockstate_plants_119),
+    pot_item_from_universal_blockstate_bedrock(b_blockstate_plants_119),
+)
+
+_B11980 = TranslationFile(
+    pot_item_to_universal_blockstate_bedrock(b_blockstate_plants_11980),
+    pot_item_from_universal_blockstate_bedrock(b_blockstate_plants_11980),
+)
+
+_B12080 = TranslationFile(
+    pot_item_to_universal_blockstate_bedrock(
+        {**b_blockstate_plants_113_saplings, **b_blockstate_plants_12080}
+    ),
+    pot_item_from_universal_blockstate_bedrock(b_blockstate_plants_12080),
+)
+
+_B12150 = TranslationFile(
+    pot_item_to_universal_blockstate_bedrock(b_blockstate_plants_12150),
+    pot_item_from_universal_blockstate_bedrock(b_blockstate_plants_12150),
+)
+
+_B2610 = TranslationFile(
+    pot_item_to_universal_blockstate_bedrock(b_blockstate_plants_2610),
+    pot_item_from_universal_blockstate_bedrock(b_blockstate_plants_2610),
 )
 
 j19 = merge(
@@ -418,11 +494,95 @@ b19 = merge(
 )
 
 b113 = merge(
-    [EmptyNBT(":FlowerPot"), _B113, bedrock_is_movable],
+    [EmptyNBT(":FlowerPot"), _B113_base, _B113_saplings, bedrock_is_movable],
     ["universal_minecraft:flower_pot"],
 )
 
 b116 = merge(
-    [EmptyNBT(":FlowerPot"), _B113, _B116, bedrock_is_movable],
+    [EmptyNBT(":FlowerPot"), _B113_base, _B113_saplings, _B116, bedrock_is_movable],
+    ["universal_minecraft:flower_pot"],
+)
+
+b117 = merge(
+    [
+        EmptyNBT(":FlowerPot"),
+        _B113_base,
+        _B113_saplings,
+        _B116,
+        _B117,
+        bedrock_is_movable,
+    ],
+    ["universal_minecraft:flower_pot"],
+)
+
+b119 = merge(
+    [
+        EmptyNBT(":FlowerPot"),
+        _B113_base,
+        _B113_saplings,
+        _B116,
+        _B117,
+        _B119,
+        bedrock_is_movable,
+    ],
+    ["universal_minecraft:flower_pot"],
+)
+
+b11980 = merge(
+    [
+        EmptyNBT(":FlowerPot"),
+        _B113_base,
+        _B113_saplings,
+        _B116,
+        _B117,
+        _B119,
+        _B11980,
+        bedrock_is_movable,
+    ],
+    ["universal_minecraft:flower_pot"],
+)
+
+b12080 = merge(
+    [
+        EmptyNBT(":FlowerPot"),
+        _B113_base,
+        _B116,
+        _B117,
+        _B119,
+        _B11980,
+        _B12080,
+        bedrock_is_movable,
+    ],
+    ["universal_minecraft:flower_pot"],
+)
+
+b12150 = merge(
+    [
+        EmptyNBT(":FlowerPot"),
+        _B113_base,
+        _B116,
+        _B117,
+        _B119,
+        _B11980,
+        _B12080,
+        _B12150,
+        bedrock_is_movable,
+    ],
+    ["universal_minecraft:flower_pot"],
+)
+
+b2610 = merge(
+    [
+        EmptyNBT(":FlowerPot"),
+        _B113_base,
+        _B116,
+        _B117,
+        _B119,
+        _B11980,
+        _B12080,
+        _B12150,
+        _B2610,
+        bedrock_is_movable,
+    ],
     ["universal_minecraft:flower_pot"],
 )
